69f5167c86f5118c3f0b2bb9f374b076fcda6583
Hafiz <komefumi@protonmail.ch>
2023-12-18 16:13:25 +0530

    Retrieves token once and reuses, by closure

--- assets/public/scripts/storage.js
+++ assets/public/scripts/storage.js
@@ -1,9 +1,14 @@
 const LOGIN_TOKEN_STORAGE_KEY = "LOGIN_TOKEN_STORAGE_KEY";
 
+let token = null;
+
 export const saveLoginToken = (tokenString) => {
   localStorage.setItem(LOGIN_TOKEN_STORAGE_KEY, tokenString);
 };
 
 export const loadLoginToken = () => {
-  localStorage.getItem(LOGIN_TOKEN_STORAGE_KEY);
+  if (token == null) {
+    token = localStorage.getItem(LOGIN_TOKEN_STORAGE_KEY);
+  }
+  return token;
 };
